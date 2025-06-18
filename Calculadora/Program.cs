f38b18c7500c111c31380250b7eef9ea802787aa
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-18 01:39:07 -0300

    mejora en la logica del bucle para continuar

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,8 +1,9 @@
-﻿using EspacioCalculadora;
+﻿using System.Runtime.CompilerServices;
+using EspacioCalculadora;
 
 // variables para el programa
 double numero = 0, opcion = 0;
-string operacion;
+string operacionMenu;
 Calculadora calculador = new Calculadora();
 
 // estructura loggica para el programa
@@ -16,24 +17,25 @@ do{
     Console.WriteLine("\t4. Dividir");
     Console.WriteLine("\t5. Limpiar");
     Console.Write("\n\tElija una opcion (ENTER PARA CANCELAR):\t");
-    operacion = Console.ReadLine();     /* lectura de la opcion elegida del menu */
+    operacionMenu = Console.ReadLine();     /* lectura de la opcion elegida del menu */
 
-    if(string.IsNullOrEmpty(operacion))break; /* verificacion de qque si se cancelo la operacion */
+    if(string.IsNullOrEmpty(operacionMenu))break; /* verificacion de qque si se cancelo la operacion */
 
     // verificacion que no se ingrese un string u opcion invalida
-    if(!double.TryParse(operacion, out opcion) || (opcion < 1 || opcion > 5)){
+    if(!double.TryParse(operacionMenu, out opcion) || (opcion < 1 || opcion > 5)){
         Console.WriteLine("\t\t---ERROR OPCION NO VALIDA, REINGRESE---");
-        continue;   /* salto al proximo ciclo en caso de que la entrada sea invalida */
+        continue; /* salto al proximo ciclo en caso de que la entrada sea invalida */
     }
 
     
     do{
         Console.Write("\n\tDesea continuar? (Si = 1 | No = 2):\t");
-        if(!double.TryParse(Console.ReadLine(), out opcion) || (opcion != 1 && opcion != 2)){
-           Console.WriteLine("\t\t---OPCION NO VALIDA, REINGRESE---");
-           continue;
+        int continuar;
+        if(int.TryParse(Console.ReadLine(), out continuar) && (continuar == 1 || continuar == 2)){
+           break;
         }
-    }while(opcion != 1 && opcion != 2);
+        Console.WriteLine("\t\t---OPCION NO VALIDA, REINGRESE---");
+    }while(true); /* no es necesario cambiar la condicion pues cuando se cumpla se rompe el bucle */
 
     Console.WriteLine(new string('-', 50)); // separador visual
-}while(opcion == 1);
+}while(continuar == 1);
