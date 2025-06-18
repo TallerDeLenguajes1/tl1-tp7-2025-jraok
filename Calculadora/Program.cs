fe0120364a777d1b3b5f4b397c1d245490f4545a
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-18 01:17:15 -0300

    primera verificacion de la entrada y el bucle para continuar

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -18,5 +18,22 @@ do{
     Console.Write("\n\tElija una opcion (ENTER PARA CANCELAR):\t");
     operacion = Console.ReadLine();     /* lectura de la opcion elegida del menu */
 
+    if(string.IsNullOrEmpty(operacion))break; /* verificacion de qque si se cancelo la operacion */
 
-}while();
+    // verificacion que no se ingrese un string u opcion invalida
+    if(!double.TryParse(operacion, out opcion) || (opcion < 1 || opcion > 5)){
+        Console.WriteLine("\t\t---ERROR OPCION NO VALIDA, REINGRESE---");
+        continue;   /* salto al proximo ciclo en caso de que la entrada sea invalida */
+    }
+
+    
+    do{
+        Console.Write("\n\tDesea continuar? (Si = 1 | No = 2):\t");
+        if(!double.TryParse(Console.ReadLine(), out opcion) || (opcion != 1 && opcion != 2)){
+           Console.WriteLine("\t\t---OPCION NO VALIDA, REINGRESE---");
+           continue;
+        }
+    }while(opcion != 1 && opcion != 2);
+
+    Console.WriteLine(new string('-', 50)); // separador visual
+}while(opcion == 1);
