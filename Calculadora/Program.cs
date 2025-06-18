90477c7a4b5d8c1ad9a4668cc573eb6dd2f961af
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-18 01:16:03 -0300

    creacion del menu de opciones y variables necesarias

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using EspacioCalculadora;
+
+// variables para el programa
+double numero = 0, opcion = 0;
+string operacion;
+Calculadora calculador = new Calculadora();
+
+// estructura loggica para el programa
+do{
+    // menu de opciones
+    Console.WriteLine("\n\t\t----Taller de lenguajes 1----");
+    Console.WriteLine("\tMenu de opciones");
+    Console.WriteLine("\t1. Sumar");
+    Console.WriteLine("\t2. Restar");
+    Console.WriteLine("\t3. Multiplicar");
+    Console.WriteLine("\t4. Dividir");
+    Console.WriteLine("\t5. Limpiar");
+    Console.Write("\n\tElija una opcion (ENTER PARA CANCELAR):\t");
+    operacion = Console.ReadLine();     /* lectura de la opcion elegida del menu */
+
+
+}while();
