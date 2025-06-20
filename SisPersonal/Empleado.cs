1744e45284bed5551d8498a3bb34b2e87a0c748b
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-20 20:04:56 -0300

    agregue logica a los metodos para verificar las entradas

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -32,9 +32,32 @@ namespace EspacioEmpleado
         public Cargos CargoEmpleado => cargoEmpleado;
 
         // metodos para cargar los datos
-        public void AsignarNombre(string nuevoNombre) => nombre = nuevoNombre;
-        public void AsignarApellido(string nuevoApellido) => apellido = nuevoApellido;
-        public void AsignarEC(char estado) => estadoCivil = estado;
+        public void AsignarNombre(string nuevoNombre) {
+            if (!string.IsNullOrEmpty(nuevoNombre))
+            {
+                nombre = nuevoNombre;
+            }else
+            {
+                Console.WriteLine("Error no es posible asignar un valor vacio");
+            }
+        }
+        public void AsignarApellido(string nuevoApellido){
+            if (!string.IsNullOrEmpty(nuevoApellido))
+            {
+                apellido = nuevoApellido;
+            }else
+            {
+                Console.WriteLine("Error no es posible asignar un valor vacio");
+            }
+        }
+        public void AsignarEC(char estado) {
+            estado = char.ToUpper(estado);
+            if (estado == 'S' || estado == 'C' || estado == 'D' || estado == 'V'){
+                estadoCivil = estado;
+            }else{
+                Console.WriteLine("Estado invalido");
+            }
+        }
         public void AsignarSueldo(float sueldo){
             if(sueldo > 0){
                 sueldoBasico = sueldo;       
@@ -42,8 +65,29 @@ namespace EspacioEmpleado
                 Console.WriteLine("\n\t\t---Error no es posible ingresar un valor negativo");
             }
         }
-        public void AsignarFechaNacimiento(DateTime fecha) => fechaNacimiento = fecha;
-        public void AsignarFechaIngreso(DateTime fecha) => fechaIngreso = fecha;
+        public void AsignarFechaNacimiento(DateTime fecha){
+            if (DateTime.Today.AddYears(-18) >= fecha)
+            {
+                fechaNacimiento = fecha;
+            }else{
+                Console.WriteLine("\n\t\t---Error no es posible ingresar menores");
+            }
+        }
+        public void AsignarFechaIngreso(DateTime fecha){
+            if (fechaNacimiento > fecha)
+            {
+                Console.WriteLine("\n\t\t---Error el ingreso no puede ser antes del nacimiento");
+            }else if(DateTime.Today < fecha)
+                {
+                Console.WriteLine("\n\t\t---Error no es posible ingresar una fecha futura");
+            }else{
+                fechaIngreso = fecha;
+            }
+        }
         public void AsignarCargo(Cargos nuevoCargo) => cargoEmpleado = nuevoCargo;
+
+        // metodos del trabajo practico
+
+
     }
 }
