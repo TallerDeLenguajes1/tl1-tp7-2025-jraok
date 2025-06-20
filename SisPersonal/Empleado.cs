36802fd2d2ed52e24725a12fcc017b5d54b5d7bb
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-20 16:23:26 -0300

    nuevos metodos para asignar valores a los campos

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Runtime.CompilerServices;
+using Internal;
 
 namespace EspacioEmpleado
 {
+    // enum con los distintos cargos de un empleado
     public enum Cargos{
         Auxiliar,
         Administrativo,
@@ -9,13 +12,17 @@ namespace EspacioEmpleado
         Especialista,
         Investigador
     }
+
+    // clase para el obejeto empleado
     public class Empleado{
+        // campos del objeto
         private string nombre, apellido;
         private DateTime fechaNacimiento, fechaIngreso;
         private char estadoCivil;
         private float sueldoBasico;
         private Cargos cargoEmpleado;
 
+        //propiedades para de solo lectura para los campos
         public string Nombre => nombre;
         public string Apellido => apellido;
         public char EstadoCivil => estadoCivil;
@@ -24,5 +31,19 @@ namespace EspacioEmpleado
         public float SueldoBasico => sueldoBasico;
         public Cargos CargoEmpleado => cargoEmpleado;
 
+        // metodos para cargar los datos
+        public void AsignarNombre(string nuevoNombre) => nombre = nuevoNombre;
+        public void AsignarApellido(string nuevoApellido) => apellido = nuevoApellido;
+        public void AsignarEC(char estado) => estadoCivil = estado;
+        public void AsignarSueldo(float sueldo){
+            if(sueldo > 0){
+                sueldoBasico = sueldo;       
+            }else{
+                Console.WriteLine("\n\t\t---Error no es posible ingresar un valor negativo");
+            }
+        }
+        public void AsignarFechaNacimiento(DateTime fecha) => fechaNacimiento = fecha;
+        public void AsignarFechaIngreso(DateTime fecha) => fechaIngreso = fecha;
+        public void AsignarCargo(Cargos nuevoCargo) => cargoEmpleado = nuevoCargo;
     }
 }
