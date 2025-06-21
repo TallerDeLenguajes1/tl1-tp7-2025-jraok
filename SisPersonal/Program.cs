d3401abb20fd072d6e9bf9a83532ed81d9fb0f33
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-21 20:00:57 -0300

    creacion del arreglo de empleados y el bucle para 3 empleados

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,4 +1,16 @@
 ﻿using EspacioEmpleado;
+using Internal;
 
 Empleado[] empleados = new Empleado[3];
 
+Console.WriteLine("\n\t\t---TALLER DE LENGUAJES I---");
+Console.WriteLine("\n\tCARGA DE DATOS DE LOS EMPLEADOS");
+for (int i = 0; i < empleados.Length; i++)
+{
+    Console.WriteLine($"\n\t\tCargando los datos del empleado {i+1}");
+    empleados[i] = new Empleado();
+    bool ok = 0;
+    string entrada;
+    
+
+}
