fe3a4165510312f7cb458dcb3138a9d89363b78c
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-21 20:12:01 -0300

    primeros bucles para asignar nombres y apellidos

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -11,6 +11,47 @@ for (int i = 0; i < empleados.Length; i++)
     empleados[i] = new Empleado();
     bool ok = 0;
     string entrada;
-    
+
+    // Ingreso el nombre del empleado
+    do
+    {
+        Console.Write("\n\tNombre:\t");
+        entrada = Console.ReadLine();
+        if (empleados[i].AsignarNombre(entrada))
+        {
+            break;
+        }else
+        {
+            Console.WriteLine("\n\t\t---Error no es posible asignar un valor vacio");
+        }
+    } while (true);
+
+    do
+    {
+        Console.Write("\n\tApellido:\t");
+        entrada = Console.ReadLine();
+        if(empleados[i].AsignarApellido(entrada)){
+            break;
+        }else
+        {
+            Console.WriteLine("\n\t\t---Error no es posible asignar un valor vacio");
+        }
+    } while (true);
+
+    do
+    {
+        Console.Write("\n\tApellido:\t");
+        entrada = Console.ReadLine();
+    } while (true);
+    do
+    {
+        Console.Write("\n\tApellido:\t");
+        entrada = Console.ReadLine();
+    } while (true);
+    do
+    {
+        Console.Write("\n\tApellido:\t");
+        entrada = Console.ReadLine();
+    } while (true);
 
 }
