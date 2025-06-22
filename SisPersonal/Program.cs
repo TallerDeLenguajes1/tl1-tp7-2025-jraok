cc01f20d8d2847f8376b69c3ab974ad015cf88ff
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-06-22 02:15:45 -0300

    case 2 listo, comentarios con la precarga de 3 objetos y tabla para todos los empleados

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using EspacioEmpleado;
-using Internal;
 
 Empleado[] empleados = new Empleado[3];     /* arreglo de objetos */
 int opcion;     /* entero auxiliar para el menu de opciones */
@@ -107,6 +106,33 @@ for (int i = 0; i < empleados.Length; i++)
 
 }
 
+// // Empleado 1: Ingeniero, casado
+// empleados[0] = new Empleado();
+// empleados[0].AsignarNombre("Laura");
+// empleados[0].AsignarApellido("Gómez");
+// empleados[0].AsignarEC('C'); // casado
+// empleados[0].AsignarFechaNacimiento(new DateTime(1975, 3, 15)); // 15/03/1975
+// empleados[0].AsignarFechaIngreso(new DateTime(2000, 6,  1));    // 01/06/2000
+// empleados[0].AsignarCargo(Cargos.Ingeniero);
+
+// // Empleado 2: Auxiliar, soltero
+// empleados[1] = new Empleado();
+// empleados[1].AsignarNombre("Marcos");
+// empleados[1].AsignarApellido("Pérez");
+// empleados[1].AsignarEC('S'); // soltero
+// empleados[1].AsignarFechaNacimiento(new DateTime(1990, 7, 22)); // 22/07/1990
+// empleados[1].AsignarFechaIngreso(new DateTime(2018, 2, 10));    // 10/02/2018
+// empleados[1].AsignarCargo(Cargos.Auxiliar);
+
+// // Empleado 3: Especialista, viudo
+// empleados[2] = new Empleado();
+// empleados[2].AsignarNombre("Ana");
+// empleados[2].AsignarApellido("Martínez");
+// empleados[2].AsignarEC('V'); // viudo
+// empleados[2].AsignarFechaNacimiento(new DateTime(1982, 11, 5)); // 05/11/1982
+// empleados[2].AsignarFechaIngreso(new DateTime(2010, 9, 12));    // 12/09/2010
+// empleados[2].AsignarCargo(Cargos.Especialista);
+
 do
 {
     Console.WriteLine("\n\t\t---MENU DE OPCIONES---");
@@ -116,23 +142,45 @@ do
     Console.WriteLine("\n\t4-Salir");
     Console.Write("\n\tOPCION:\t");
     entrada = Console.ReadLine();
-    if (int.TryParse(entrada, out opcion) || opcion < 1 || opcion > 4){
+    if (!int.TryParse(entrada, out opcion) || opcion < 1 || opcion > 4){
         Console.WriteLine("\n\tOpcion Invalida, REINGRESE");
         continue;
     }
     switch (opcion)
     {
         case 1:
-            float salarios;
+            float salarios = 0.0f;
             for (int i = 0; i < empleados.Length; i++)
             {
                 salarios+=empleados[i].CalcularSueldo();
             }
+            Console.WriteLine($"Total a pagar en concepto de salarios: {salarios}");
             break;
-        case 1:
+        case 2:
+            int indice = 0, edadBase = 0;
+            for (int i = 0; i < empleados.Length; i++)
+            {
+                if (empleados[i].CalcularEdad() > edadBase)
+                { 
+                    edadBase = empleados[i].CalcularEdad();
+                    indice = i;
+                }
+            }
+            Console.WriteLine("\n\tEMPLEADO MAS PROXIMO A JUBILARSE");
+            Console.WriteLine($"\n\tNombre completo: {empleados[indice].Nombre} {empleados[indice].Apellido}");
+            Console.WriteLine($"\n\tEdad: {edadBase} años");
+            Console.WriteLine($"\n\tCargo del empleado: {empleados[indice].CargoEmpleado}");
+            Console.WriteLine($"\n\tAntiguedad: {empleados[indice].CalcularAntiguedad()} años");
+            Console.WriteLine($"\n\tSalario: ${empleados[indice].CalcularSueldo()}");
             break;
-        case 1:
-            break;    
+        // case 3:
+        //     Console.WriteLine("\n\t\t---EMPLEADOS---");
+        //     Console.WriteLine($"\n\t{"NOMBRE",-15} {"APELLIDO",-11} {"ESTADO CIVIL",-14} {"EDAD",-5} {"ANTIGüEDAD",-8} {"CARGO",15} {"SUELDO",10}\n");
+        //     foreach (Empleado obj in empleados)
+        //     {
+        //         Console.Write($"\t{obj.Nombre,-15} {obj.Apellido,-11} {obj.EstadoCivil,-14} {obj.CalcularEdad(),-5} {obj.CalcularAntiguedad() + " años",-13} {obj.CargoEmpleado, -15} {obj.CalcularSueldo(),10:F2}\n");
+        //     }
+        //     break;
     }
 
 } while (opcion != 4);
